0c017ba04a9fca767150e33e5805348822fcd75a
asadeg02 <arezu.sadeghi@gmail.com>
2022-02-28 21:04:18 -0500

    Add version 2 data and features

--- js/config.js
+++ js/config.js
@@ -1,63 +1,27 @@
 export let config = {
 	'accessToken':'pk.eyJ1IjoiaGFyaXJpc2FpbCIsImEiOiJjazZ3aGF2MmkwN2pvM21vYmZtYTJrOG13In0.4IxSwTcKWq1ms7F-B8v2Iw',
-	'dataPath': 'data/states-careers.json',	
-	//'propertiesToIds': {
-	//	'State ILP': {'HTMLId': /*'stateILP'*/'stateILPProces','isLink': false},
-	//	'ILP Webpage': {'HTMLId': /*'ILPWebpage'*/'stateILPProces','isLink': true},
-	//	'ILP Contact Name + Email': {'HTMLId': /*'ILPInfo'*/'stateILPProces','isLink': false},
-	//	'ILP Policy': {'HTMLId': /*'ILPPolicy'*/'stateILPProces', 'isLink': true},
-	//	'ILP Guide': {'HTMLId': /*'ILPGuide'*/'stateILPProces','isLink': true},
-	//	'State K12 Equity Plan': {'HTMLId': /*'equityPlan'*/'ILPDEI','isLink': true},
-	//	'State ESSA Plan': {'HTMLId': /*'stateESSAPlan'*/'ILPDEI','isLink': true},
-	//	'ILP Equity Reference ': {'HTMLId': /*'ILPEquityReference'*/'ILPDEI','isLink': true},	
-	//	'ILP Funding': {'HTMLId': 'ILPFunding','isLink': true},
-	//	'ILP PD/Training': {'HTMLId': /*'PD/Training'*/'ILPSupport','isLink': true},
-	//	'ILP Standards': {'HTMLId': /*'ILPStandards'*/'ILPSupport','isLink': true},	
-	//	'ILP Curriculum &/or Lessons': {'HTMLId': /*'ILPCurriculum'*/'ILPSupport','isLink': true},
-	//	'ILP Tech Platform': {'HTMLId': /*'techPlatform'*/'ILPTechPlatform','isLink': true},
-	//},
+	'dataPath': 'data/states-careers2.json',	
 	'propertiesToIds': {
-		'stateILPProces': [
-			{'key': 'State ILP','isLink': false},
-			{'key': 'ILP Webpage','isLink': true},
-			{'key': 'ILP Contact Name + Email','isLink': false},
-			{'key': 'ILP Policy','isLink': true},
-			{'key': 'ILP Guide','isLink': true},
-		],
-		'ILPDEI': [
-			{'key': 'State K12 Equity Plan','isLink': true},
-			{'key': 'State ESSA Plan','isLink': true},
-			{'key': 'ILP Equity Reference','isLink': true}			
-		],
-		'ILP Funding': [
-			{'key': 'ILP Funding','isLink': true}					
-		],
-		'ILPSupport': [
-			{'key': 'ILP PD/Training','isLink': true},
-			{'key': 'ILP Standards','isLink': true},
-			{'key': 'ILP Curriculum &/or Lessons','isLink': true}			
-		],
-		'ILPTechPlatform': [
-			{'key': 'ILP Tech Platform','isLink': true}					
-		],
-		'SEL': [
-			{'key': 'State SEL Webpage','isLink': true},
-			{'key': 'SEL in ILP Reference','isLink': true}					
-		],
-		'CTE': [
-			{'key': 'State Perkins V Plan','isLink': true},
-			{'key': 'K12 CTE','isLink': true},
-			{'key': 'K12 CTE, Career Pathways in ILP Reference','isLink': true}					
-		],
-		'dualEnrollment': [
-			{'key': 'Dual Enrollment Webpage','isLink': true}						
-		],
-		'IRCs': [
-			{'key': 'IRC Webpage','isLink': true}					
-		],
-		'WBL': [
-			{'key': 'WBL Webpage','isLink': true},
-			{'key': 'WBL Guide','isLink': true}					
-		]		
-	}
+		'PCAPName': {'key': 'State Personalized Career & Academic Plan (PCAP) Name','isLink': false},
+		'PCAPContact': {'key': 'PCAP Contact','isLink': false},
+		'PCAPWebpage': {'key': 'State PCAP Webpage','isLink': true},
+		'PCAPGuide': {'key': 'PCAP Guide', 'isLink': true},
+		'ESSAPlan': {'key': 'ESSA Plan (Equity Approach)','isLink': true},
+		'PCAPPlatform': {'key': 'PCAP Technology Platform','isLink': true},
+		'PerkinsVPlan': {'key': 'Perkins V (Career & Technical Education-CTE) Plan','isLink': true},
+		'K12CTE': {'key': 'K12 CTE','isLink': true},	
+		'K12WBL': {'key': 'K12 Work-Based Learning (WBL)','isLink': true},
+		'K12WBLGuide': {'key': 'K12 WBL Guide','isLink': true}		
+	},
+	'stateRecoms': {
+		'PCAPWebpage': ['AK', 'CO', 'KS', 'KY', 'OK', 'PA', 'WI'],
+		'PCAPGuide': ['CO', 'KS'],
+		'PCAPPlatform': ['AL', 'AK', 'MA', 'MN', 'OH', 'SD', 'WI'],
+		'ESSAPlan': ['CO', 'KS', 'OH', 'OK', 'PA', 'RI'],
+		'PerkinsVPlan': ['IN', 'RI', 'WI'],
+		'K12CTE': ['CO', 'IL', 'IA', 'MA', 'RI', 'TN', 'VA', 'WA', 'WI'],
+		'K12WBL': ['DE', 'GA', 'IA', 'MA', 'OH', 'RI'],
+		'K12WBLGuide': ['DE', 'GA', 'IA', 'MA', 'OH', 'RI']
+	}		
+	
 }
--- js/index.js
+++ js/index.js
@@ -18,11 +18,15 @@ function ajax_get(url, callback) {
     xmlhttp.send();
 }
 
-function onScoresRadioChange() {
+function onScoresRadioChange(stateRecoms) {
     $("#scores input[name='score-radio']").change(function() {
-        if ($(this).is(':checked')) {           
-            map.setPaintProperty('states-layer', 'fill-color', { 'property': $(this).val(), stops: [[0, 'red'], [1, 'red'], [2, 'green'], [3, 'green'],[4, 'blue'], [5, 'blue']] });
-             
+        if ($(this).is(':checked')) {            
+            map.setPaintProperty('states-layer', 'fill-color', [
+                                                                'case', 
+                                                                ["in", ['get', 'state_name'], ['literal', stateRecoms[$(this).val()]]], 
+                                                                '#f0ad4e', 
+                                                                '#C0C0C0']);
+            $("#dropdownBtn").text('CCD State Recomandations for: ' + $(this).val());            
         }                          
     });    
 }
@@ -39,6 +43,13 @@ function getSelectedScore() {
     return selectedScore;    
 }
 
+function resetStateRecoms() {
+
+    $("#dropdownBtn").text('CCD State Recomandations ');
+    $("#scores input[name='score-radio']").each(function () {            
+        $(this).prop('checked', false);
+    });    
+}
 
 const CONFIG = config['config'];
 let statesData = null;
@@ -49,16 +60,15 @@ ajax_get(CONFIG['dataPath'], function (d) {
 
 mapboxgl.accessToken = CONFIG['accessToken'];
 
-const map = new mapboxgl.Map({
+let map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/light-v10',
     center: [-86.5, 38.907],
     zoom: 4.1,
-    minZoom: 4.1
-    //scrollZoom      : false,
-    //boxZoom         : false,
-    //doubleClickZoom : false
+    minZoom: 4.1,    
 });
+
+map.addControl(new mapboxgl.NavigationControl(), 'top-right');
  
 map.on('load', () => {
     var $ = window["$"];
@@ -69,6 +79,15 @@ map.on('load', () => {
         'type': 'geojson',
         'data': statesData
     });
+
+    map.addLayer({
+        'id': 'bg',
+        'type': 'background',        
+        'paint': { 
+            'background-color': 'white',
+            'background-opacity': 0.8 
+        }
+    });    
  
     // Add a layer showing the state polygons.
     map.addLayer({
@@ -76,12 +95,8 @@ map.on('load', () => {
         'type': 'fill',
         'source': 'states',
         'paint': {
-        'fill-color': {
-            property: 'Statewide ILP P&G',
-            stops: [[0, 'red'], [1, 'red'], [2, 'green'], [3, 'green'],[4, 'blue'], [5, 'blue']]
-        },
-        //'fill-outline-color': 'rgba(200, 100, 240, 1)',
-        'fill-outline-color': 'black'
+            'fill-color': '#C0C0C0',        
+            'fill-outline-color': 'black'
         }
     });
 
@@ -100,107 +115,59 @@ map.on('load', () => {
         }
     });
  
-// When a click event occurs on a feature in the states layer,
-// open a popup at the location of the click, with description
-// HTML from the click event's properties.
-map.on('click', 'states-layer', (e) => {
 
-    let properties = e.features[0].properties;
-    const coordinates = e.features[0].geometry.coordinates;
-    const propToIds = CONFIG['propertiesToIds']; 
-    const stateName = properties['state_name'];
-    
-    //update the main accordion buttons titles, add the name of clicked state to them
-    const buttonIds = ["ILPPolicyBtn", "ILPSupportsBtn", "ILPProgramBtn"]
-    for (const id of buttonIds) {
-        $("#" + id).html($("#" + id).html().split("(")[0] + " (" + stateName + ")");    
-    }
-    
-    new mapboxgl.Popup()
-        .setLngLat(e.lngLat)
-        .setHTML("<h5 class='text-info mt-2'>" + getSelectedScore() + " : " + properties[getSelectedScore()] + "</h5>")
-        .addTo(map);     
+    map.on('click', 'states-layer', (e) => { 
 
-    for (const id in propToIds) {
-        let noData = true;
-        $("#" + id).empty(); 
-        const elems = propToIds[id];
-        for (let j = 0; j < elems.length; ++j) { 
-            const elem = elems[j];           
-            const links = JSON.parse(properties[elem['key']]);
-            if (elem['key'] !== 'ILP Contact Name + Email') {
-                for (let i = 0; i < links.length; ++i) {
-                    if (elem['isLink']) {                        
-                        $("#" + id).append("<a class='text-success' href='" + links[i] + "'>" + elem['key'] + " " + (i+1) + "</a><br>");
-                        noData = false;                              
-                    }                   
-                    else {                        
-                        $("#" + id).append("<p class='text-dark'>" + elem['key'] + ":" + "</p>");                        
-                        $("#" + id).append("<p class='text-secondary'>" + links[i] + "</p>");
-                        noData = false;                                                                           
-                    }
-                }
-            }
-            else if (elem['key'] === 'ILP Contact Name + Email'){
-                $("#" + id).append("<p class='text-dark'>" + elem['key'] + ":" + "</p>");
-                for (const elm of links) {
-                    $("#" + id).append("<p class='text-secondary'>" + elm + "</p>");
-                }
-                noData = false;                      
-            }
-            if (links.length > 0 && j !== elems.length-1) {
-                let isTheLast = true;
-                for (let k = j + 1; k < elems.length; ++k ) {
-                    if (JSON.parse(properties[elems[k]['key']]).length > 0) {
-                        isTheLast = false;
-                        break;
-                    }
-                }  
-                if(!isTheLast) {
-                    $("#" + id).append("<hr class='mt-1 font-weight-bold'>");    
-                }           
-            }                
-        }
-        if (noData) {
-            $("#" + id).append("<p>No data available</p>");   
-        }  
-    }
-         
-    /*for (const key in properties) {
-        if (key in propToIds)
-            //$("#" + propToIds[key]['HTMLId']).empty();
-        if (key in propToIds) {            
-            for (let i = 0; i < JSON.parse(properties[key]).length; ++i) {
-                const elem = JSON.parse(properties[key])[i];
-                if(propToIds[key].isLink) {
-                    //$("#" + propToIds[key]['HTMLId']).append("<li><a href='" + elem + "'>" + "Link " + (i+1) + "</a></li>");
-                    $("#" + propToIds[key]['HTMLId']).append("<a class='text-success' href='" + elem + "'>" + key + " " + (i+1) + "</a><br>");    
-                }
-            }
-            if (JSON.parse(properties[key]).length > 0) {
-                $("#" + propToIds[key]['HTMLId']).append("<hr class='mt-1'>");
-            }
+        resetStateRecoms();           
+
+        let properties = e.features[0].properties;
+        const coordinates = e.features[0].geometry.coordinates;
+        const propToIds = CONFIG['propertiesToIds']; 
+        const stateName = properties['state_name'];
+
+        map.setPaintProperty('states-layer', 'fill-color', ['case', ["==", ['get', 'state_name'], stateName], '#5bc0de', '#C0C0C0']);    
+
+        $("#stateName").removeClass("d-none").empty().text("Links for " + stateName);        
+        $("#links").empty()
+        for (const id in propToIds) {
+
+            $("#links").append("<ul id='" + id + "'></ul>");
             
-        }
-    }*/
+            const key = propToIds[id]['key'];
+            const links = JSON.parse(properties[key]);        
 
-});
+            $("#" + id).append("<p class='text-dark font-weight-bold'>" + key + "</p>");
+            if (links.length === 0) {            
+                $("#" + id).append("<p class='text-secondary'>No resource found</p>")
+            } 
+            if (propToIds[id]['isLink']) {            
+                for (let i = 0; i < links.length; ++i) {                                                               
+                    $("#" + id).append("<li><a target='_blank' class='text-success' href='" + links[i] + "'>" + key + " " + (i+1) + "</a></li><br>");                                                
+                }
+            }
+            else {                                      
+                for (const elm of links) {
+                    $("#" + id).append("<li><p class='text-secondary'>" + elm + "</p></li>");
+                }
+                                     
+            }
+            $("#links").append("<hr class='mt-1 font-weight-bold'>");        
+        }  
+
+    });
  
-// Change the cursor to a pointer when
-// the mouse is over the states layer.
-map.on('mouseenter', 'states-layer', () => {
-    map.getCanvas().style.cursor = 'pointer';
-});
+
+    map.on('mouseenter', 'states-layer', () => {
+        map.getCanvas().style.cursor = 'pointer';
+    });
  
-// Change the cursor back to a pointer
-// when it leaves the states layer.
-map.on('mouseleave', 'states-layer', () => {
-    map.getCanvas().style.cursor = '';
-});
+
+    map.on('mouseleave', 'states-layer', () => {
+        map.getCanvas().style.cursor = '';
+    });
 });
 
 
-$(document).ready(function(){
-    $("#instructionsModal").modal('show'); 
-    onScoresRadioChange();   
+$(document).ready(function(){     
+    onScoresRadioChange(CONFIG['stateRecoms']);   
 });
