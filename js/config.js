8026f1ee7d006ed665fdb0e5e9338bbdb410e3d4
asadeg02 <arezu.sadeghi@gmail.com>
2022-10-12 03:20:02 -0400

    Update geojson file

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export let config = {
 	'accessToken':'pk.eyJ1IjoiaGFyaXJpc2FpbCIsImEiOiJjazZ3aGF2MmkwN2pvM21vYmZtYTJrOG13In0.4IxSwTcKWq1ms7F-B8v2Iw',
-	'dataPath': 'data/states-careers5.json',	
+	'dataPath': 'data/states-careers-2022-9.json', //'data/states-careers5.json',	
 	'propertiesToIds': {
 		'PCAPName': {'key': 'PCAP Name','isLink': false},
 		'PCAPContact': {'key': 'PCAP Contact','isLink': false},
