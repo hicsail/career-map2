0d73c5eafedc5eb3000e2838f53e79af8c2c8483
wenhwang97 <wenhwang@bu.edu>
2023-09-21 18:02:16 -0400

    display score and fix coloring

--- js/score.js
+++ js/score.js
@@ -29,16 +29,20 @@ function createColorScale(color, scale) {
   const green = parseInt(color.slice(3, 5), 16);
   const blue = parseInt(color.slice(5, 7), 16);
 
-  const deltaRed = (red - 255) / (scale - 1);
-  const deltaGreen = (green - 255) / (scale - 1);
-  const deltaBlue = (blue - 255) / (scale - 1);
+  const deltaRed = (red - 255) / scale;
+  const deltaGreen = (green - 255) / scale;
+  const deltaBlue = (blue - 255) / scale;
 
   const colorScale = [];
-  for (let i = 0; i < scale; i++) {
+  for (let i = 0; i <= scale; i++) {
     const newRed = Math.round(255 + i * deltaRed);
     const newGreen = Math.round(255 + i * deltaGreen);
     const newBlue = Math.round(255 + i * deltaBlue);
-    colorScale.push(`#${newRed.toString(16)}${newGreen.toString(16)}${newBlue.toString(16)}`);
+    colorScale.push(
+      `#${newRed.toString(16).padStart(2, "0")}${newGreen.toString(16).padStart(2, "0")}${newBlue
+        .toString(16)
+        .padStart(2, "0")}`
+    );
   }
 
   return colorScale;
@@ -53,13 +57,10 @@ function getLuminance(color) {
 }
 
 function onHoverState(state, abbr) {
-  console.log(state);
-
-  $("#col").find("span:first").text(state);
+  $("#bottom-dash .state-name").find("span").text(state);
   for (const indicator of ["youth", "adulthood1", "adulthood2", "social"]) {
-    $(`#${indicator}`)
-      .empty()
-      .html(`<ul><li><span>${scoreData[abbr][indicator]["cr_score100"]}</span></li><li><span></span></li></ul>`);
+    const score = Math.round((scoreData[abbr][indicator]["cr_score100"] + Number.EPSILON) * 100) / 100;
+    $(`.state-row .${indicator}-score`).empty().html(`<span>${score}</span>`);
   }
 }
 
@@ -76,10 +77,11 @@ function paintState(theme) {
   const expression = ["match", ["get", "state_abbrev"]];
   for (const stateAbbr of Object.keys(scoreData)) {
     const score = scoreData[stateAbbr][theme]["cr_score100"];
-    if (score === 0) continue;
 
-    let level = Math.floor(score / (100 / colorPalette[theme]["scale"]));
-    level = level == colorPalette[theme]["scale"] ? level - 1 : level;
+    let level = Math.floor(score / (100 / colorPalette[theme]["scale"] + 1)) + 1;
+    if (level === colorPalette[theme]["scale"].length) level--;
+    if (score === 0) level = 0;
+
     const color = colorScale[theme][level];
 
     expression.push(stateAbbr, color);
@@ -89,6 +91,13 @@ function paintState(theme) {
   expression.push("#cdcdcd");
 
   map.setPaintProperty("state", "fill-color", expression);
+  for (let idx = colorScale[theme].length - 1; idx >= 0; idx--) {
+    const item = $("<div></div>");
+    const colorBox = $("<span></span>").css("background-color", colorScale[theme][idx]);
+    const text = $("<span></span>").text("test");
+    item.append(colorBox).append(text);
+    $("#choropleth-legend").append(item);
+  }
 }
 
 mapboxgl.accessToken = CONFIG["accessToken"];
@@ -136,6 +145,7 @@ const map = new mapboxgl.Map({
         filter: ["all", ["==", "type", "state"], ["!in", "state_abbrev", "PR", "DC"]],
         paint: {
           "line-color": "#6b6b6b",
+          "line-opacity": 0.7,
         },
       },
       {
@@ -181,6 +191,23 @@ map.on("load", () => {
   map.fitBounds(boundingBox);
   paintState(currentTheme);
 
+  for (const theme of ["youth", "adulthood1", "adulthood2", "social"]) {
+    let accumScore = 0;
+    let numStates = 50;
+    for (const stateAbbr of Object.keys(scoreData)) {
+      if (scoreData[stateAbbr][theme]["cr_score100"] === null) {
+        numStates--;
+        continue;
+      }
+
+      accumScore += scoreData[stateAbbr][theme]["cr_score100"];
+    }
+
+    const nationalScore = Math.round((accumScore / numStates + Number.EPSILON) * 100) / 100;
+
+    $(`.national-row .${theme}-score`).empty().html(`<span>${nationalScore}</span>`);
+  }
+
   map.on("mousemove", "state", (e) => {
     const state = e.features[0].properties.state_name;
     const stateAbbrev = e.features[0].properties.state_abbrev;
@@ -198,4 +225,10 @@ map.on("load", () => {
   map.on("mouseleave", "state", () => {
     map.getCanvas().style.cursor = "";
   });
+});
+
+$("#state-legends .state-badge").hover(function () {
+  const name = $(this).attr("name");
+  const abbr = $(this).text();
+  onHoverState(name, abbr);
 });
